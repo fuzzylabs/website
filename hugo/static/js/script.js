f32f6c5e15e80c5c8f9c37f371ef673a8e07de05
archena <archena@users.noreply.github.com>
2020-12-01 12:01:02 +0000

    Mobile navigation fixes part 2: dropdown menus

--- hugo/static/js/script.js
+++ hugo/static/js/script.js
@@ -13,4 +13,26 @@ $(function () {
     if (window.location.pathname === '/' && !window.location.hash) {
         pageLinks.filter('[href*="home"]').addClass('selected');
     }
+
+    // Dropdown behaviour
+    
+    const $dropdown = $(".dropdown");
+    const $dropdownToggle = $(".dropdown-toggle");
+    const $dropdownMenu = $(".dropdown-menu");
+    const showClass = "show";
+
+    $dropdown.hover(
+        function() {
+            const $this = $(this);
+            $this.addClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "true");
+            $this.find($dropdownMenu).addClass(showClass);
+        },
+        function() {
+            const $this = $(this);
+            $this.removeClass(showClass);
+            $this.find($dropdownToggle).attr("aria-expanded", "false");
+            $this.find($dropdownMenu).removeClass(showClass);
+        }
+    );
 });
